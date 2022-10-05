e1cb5e97ac734557f7ecbd5604aa9d9a350fec20
Cian O'Gorman <29048772+CianOG123@users.noreply.github.com>
2022-10-05 22:51:06 +0100

    Created string verification function
    Create function to verify if strings are correctly formed and unit tests to support this.

--- calculator.py
+++ calculator.py
@@ -2,6 +2,22 @@ def main():
     print("Hello World")
     print(sub_calc(1, 2, "*"))
 
+def verifyString(input):
+    symbol = False
+    for c in input:
+        if(c == '+' or c == '-' or c == '*'):
+            if(symbol == True):
+                return False
+            else:
+                symbol = True
+        elif(ord(c) >= ord('0') and ord(c) <= ord('9')):
+            symbol = False
+        else:
+            return False
+    if(symbol == False):
+        return True
+    return False
+
 def precedence(operators):
     if operators == '+' or operators == '-': return 1
     if operators == '*' or operators == '/': return 2
