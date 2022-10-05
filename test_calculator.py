2b5f4c34b9e65b6bd4eedc18b34105c87b3bb4b5
okaforc <okaforc@tcd.ie>
2022-10-05 22:24:44 +0100

    switched to pytest

--- test_calculator.py
+++ test_calculator.py
@@ -1,70 +1,66 @@
-import unittest
 from calculator import *
 
-class Testing(unittest.TestCase):
-    # Test if precedence is correctly handled
-    def testPrecedence(self):
-        input = "+"
-        expected = 1
-        result = precedence(input)
-        self.assertEqual(expected, result)
 
-        input = "-"
-        expected = 1
-        result = precedence(input)
-        self.assertEqual(expected, result)
+# Test if precedence is correctly handled
+def testPrecedence():
+    input = "+"
+    expected = 1
+    result = precedence(input)
+    assert expected == result
 
-        input = "*"
-        expected = 2
-        result = precedence(input)
-        self.assertEqual(expected, result)
+    input = "-"
+    expected = 1
+    result = precedence(input)
+    assert expected == result
 
-    # Test if subCalc can multiply
-    def testSubCalcMultiplication(self):
-        expected = 108
-        result = sub_calc(12, 9, '*')
-        self.assertEqual(expected, result)
+    input = "*"
+    expected = 2
+    result = precedence(input)
+    assert expected == result
 
-        expected = 21
-        result = sub_calc(3, 7, '*')
-        self.assertEqual(expected, result)
+# Test if subCalc can multiply
+def testSubCalcMultiplication():
+    expected = 108
+    result = sub_calc(12, 9, '*')
+    assert expected == result
 
-        expected = 0
-        result = sub_calc(483, 0, '*')
-        self.assertEqual(expected, result)
+    expected = 21
+    result = sub_calc(3, 7, '*')
+    assert expected == result
 
-        expected = 1
-        result = sub_calc(1, 1, '*')
-        self.assertEqual(expected, result)
-        
-    # Test if subCalc can add
-    def testSubCalcAddition(self):
-        expected = 9
-        result = sub_calc(9, 0, '+')
-        self.assertEqual(expected, result)
+    expected = 0
+    result = sub_calc(483, 0, '*')
+    assert expected == result
 
-        expected = 32
-        result = sub_calc(5, 27, '+')
-        self.assertEqual(expected, result)
+    expected = 1
+    result = sub_calc(1, 1, '*')
+    assert expected == result
+    
+# Test if subCalc can add
+def testSubCalcAddition():
+    expected = 9
+    result = sub_calc(9, 0, '+')
+    assert expected == result
 
-        expected = 0
-        result = sub_calc(0, 0, '+')
-        self.assertEqual(expected, result)
+    expected = 32
+    result = sub_calc(5, 27, '+')
+    assert expected == result
 
-    # Test if subCalc can subtract
-    def testSubCalcSubtraction(self):
-        expected = 9
-        result = sub_calc(9, 0, '-')
-        self.assertEqual(expected, result)
+    expected = 0
+    result = sub_calc(0, 0, '+')
+    assert expected == result
 
-        expected = 21
-        result = sub_calc(26, 5, '-')
-        self.assertEqual(expected, result)
+# Test if subCalc can subtract
+def testSubCalcSubtraction():
+    expected = 9
+    result = sub_calc(9, 0, '-')
+    assert expected == result
 
-        expected = -5
-        result = sub_calc(5, 10, '-')
-        self.assertEqual(expected, result)
+    expected = 21
+    result = sub_calc(26, 5, '-')
+    assert expected == result
 
+    expected = -5
+    result = sub_calc(5, 10, '-')
+    assert expected == result
 
-if __name__ == '__main__':
-    unittest.main()
