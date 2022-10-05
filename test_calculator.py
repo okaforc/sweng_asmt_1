d8571427835b65d81da0786716ae7dea4b9793a8
okaforc <okaforc@tcd.ie>
2022-10-05 23:26:02 +0100

    added stack evaluation functions

--- test_calculator.py
+++ test_calculator.py
@@ -117,4 +117,32 @@ def testAppendDigits():
     values = []
     i, inputString, values = appendDigits(i, inputString, values)
     assert expected == values[0]
+    
+# Test if evaluate can handle addition with multiple arguments
+# def testAddition():
+#     input = "45+23+101"
+#     expected = 169
+#     result = evaluate(input)
+#     self.assertEqual(expected, result)
 
+#     input = "5+300+20"
+#     expected = 325
+#     result = evaluate(input)
+#     self.assertEqual(expected, result)
+
+# Test evaluate with multiple arguments
+# def testEvaluate():
+#     input = "45+23-101*2"
+#     expected = -134
+#     result = evaluate(input)
+#     self.assertEqual(expected, result)
+
+#     input = "5+300*0"
+#     expected = 5
+#     result = evaluate(input)
+#     self.assertEqual(expected, result)
+
+#     input = "5*12-6"
+#     expected = 54
+#     result = evaluate(input)
+#     self.assertEqual(expected, result)
